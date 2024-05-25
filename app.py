013414684fedc2ed5bad1b2309aec5829abb808a
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2024-05-25 22:24:12 +0300

    app's code updated

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ counter = 0  # счетчик повторений
 
 with open('output.log', 'a') as logfile:  # открываем файл для добавления текста
     while True:
-        counter += 1
-        log_message = f"Приложение работает. Повторение №{counter}\n"
+        counter += 2
+        log_message = f"Приложение обновлено и работает. CI/CD рулит. Повторение №{counter}\n"
         print(log_message)  # вывод сообщения в стандартный поток вывода (stdout)
         logfile.write(log_message)  # запись сообщения в файл
-        time.sleep(3)  # ожидание 1 секунды
+        time.sleep(1)  # ожидание 1 секунды
